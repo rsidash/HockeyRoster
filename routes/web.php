9fad57d9d756cd37c1ebb52e00adde8bf8cfeebc
Roman Sidash <sidash.roman@mail.ru>
2023-05-28 00:17:56 +0600

    refactor: add auth & teams routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [MainController::class, 'index'])->name('main');
+
 Route::resource('teams', TeamController::class)->only([
     'index', 'show', 'create', 'store',
 ]);
+
+Route::resource('players', TeamController::class)->only([
+    'index', 'show', 'create', 'store',
+]);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('auth.create');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
+
+    Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
+    Route::post('/register', [RegisterController::class, 'register'])->name('register.store');
+
+    Route::get('/reset-password', [ResetPasswordController::class, 'create'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'store'])->name('password.update');
+
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('password.request');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('password.email');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+
+    Route::resource('teams', TeamController::class)->only([
+        'create', 'store',
+    ]);
+
+    Route::resource('players', TeamController::class)->only([
+        'create', 'store',
+    ]);
+});
