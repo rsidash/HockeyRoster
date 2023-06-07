cb30b745c98c37e1908fd3fb7c32ca32b6f38688
Roman Sidash <sidash.roman@mail.ru>
2023-06-08 00:58:53 +0600

    refactor: add avatar to blade template using ui-avatars.com/api

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -42,19 +42,23 @@
                     <div class="d-lg-none flex-shrink-0 dropdown">
                         <a href="#" class="link-secondary text-decoration-none d-flex align-items-center"
                            id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
-                            @if (isset(Auth::user()->avatar_path))
-                                <img
-                                    src="{{ Storage::url(Auth::user()->avatar_path) }}"
-                                    alt="{{ Auth::user()->email }}" width="32" height="32"
-                                    class="rounded-circle">
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-                                     fill="#0094fd" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                    <path fill-rule="evenodd"
-                                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                </svg>
-                            @endif
+                            <img
+                                src="https://ui-avatars.com/api/?name={{ Auth::user()->first_name . '+' . Auth::user()->last_name }}&rounded=true&background=random&size=32&bold=true"
+                                alt="" width="32" height="32"
+                                class="rounded-circle">
+{{--                            @if (isset(Auth::user()->avatar_path))--}}
+{{--                                <img--}}
+{{--                                    src="{{ Storage::url(Auth::user()->avatar_path) }}"--}}
+{{--                                    alt="{{ Auth::user()->email }}" width="32" height="32"--}}
+{{--                                    class="rounded-circle">--}}
+{{--                            @else--}}
+{{--                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"--}}
+{{--                                     fill="#0094fd" class="bi bi-person-circle" viewBox="0 0 16 16">--}}
+{{--                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>--}}
+{{--                                    <path fill-rule="evenodd"--}}
+{{--                                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>--}}
+{{--                                </svg>--}}
+{{--                            @endif--}}
                             <span
                                 class="d-none d-md-inline-flex align-items-center fw-bold me-1 p-2">{{ Auth::user()->email }}</span>
                         </a>
@@ -123,19 +127,23 @@
                                            id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
                                         <span
                                             class="d-flex align-items-center fw-bold mx-2">{{ Auth::user()->email }}</span>
-                                            @if (isset(Auth::user()->avatar_path))
-                                                <img
-                                                    src="{{ Storage::url(Auth::user()->avatar_path) }}"
-                                                    alt="" width="32" height="32"
-                                                    class="rounded-circle">
-                                            @else
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-                                                     fill="#0094fd" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                                    <path fill-rule="evenodd"
-                                                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                                </svg>
-                                            @endif
+                                            <img
+                                                src="https://ui-avatars.com/api/?name={{ Auth::user()->first_name . '+' . Auth::user()->last_name }}&rounded=true&background=random&size=32&bold=true"
+                                                alt="" width="32" height="32"
+                                                class="rounded-circle">
+{{--                                            @if (isset(Auth::user()->avatar_path))--}}
+{{--                                                <img--}}
+{{--                                                    src="{{ Storage::url(Auth::user()->avatar_path) }}"--}}
+{{--                                                    alt="" width="32" height="32"--}}
+{{--                                                    class="rounded-circle">--}}
+{{--                                            @else--}}
+{{--                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"--}}
+{{--                                                     fill="#0094fd" class="bi bi-person-circle" viewBox="0 0 16 16">--}}
+{{--                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>--}}
+{{--                                                    <path fill-rule="evenodd"--}}
+{{--                                                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>--}}
+{{--                                                </svg>--}}
+{{--                                            @endif--}}
                                         </a>
                                         <ul class="dropdown-menu dropdown-menu-end text-small shadow"
                                             aria-labelledby="dropdownUser" style="">
